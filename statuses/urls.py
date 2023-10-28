0233a117c86d7655ac4124ad37eb5caf7a2095f8
Alex <taus.anarchy@gmail.com>
2023-10-28 14:41:35 +0300

    Prettify code and add url for StatusDetailView

--- statuses/urls.py
+++ statuses/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import StatusCreateView, StatusEditView, StatusLikeView
+from .views import (
+    StatusCreateView,
+    StatusEditView,
+    StatusLikeView,
+    StatusDetailView,
+)
 
 urlpatterns = [
     path('create/', StatusCreateView.as_view(), name='status_create'),
     path('edit/by_status_id/<int:id>', StatusEditView.as_view(), name='status_edit_by_status_id'),
     path('edit/by_user_id/<int:user_id>', StatusEditView.as_view(), name='status_edit_by_user_id'),
-    path('like/<int:status_id>', StatusLikeView.as_view(), name='status_like')
+    path('like/<int:status_id>', StatusLikeView.as_view(), name='status_like'),
+    path('detail/<int:pk>', StatusDetailView.as_view(), name='status_detail'),
 ]

247bbdc6663abac26953156552cd9250c4e99572
Alex <taus.anarchy@gmail.com>
2023-10-28 14:42:25 +0300

    Change Status' get_absolute_url destination

--- statuses/models.py
+++ statuses/models.py
@@ -17,4 +17,4 @@ class Status(models.Model):
         return self.text[:50]
 
     def get_absolute_url(self):
-        return reverse('home')
+        return reverse('status_detail', args=[self.pk])

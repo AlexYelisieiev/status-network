6c261551751ab0e5ab86de872c83abd61118c6ee
Alex <taus.anarchy@gmail.com>
2023-11-01 08:41:04 +0200

    Add path for detail view

--- statuses/urls.py
+++ statuses/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('edit/by_status_id/<int:id>', StatusEditView.as_view(), name='status_edit_by_status_id'),
     path('edit/by_user_id/<int:user_id>', StatusEditView.as_view(), name='status_edit_by_user_id'),
     path('like/<int:status_id>', StatusLikeView.as_view(), name='status_like'),
-    path('detail/<int:pk>', StatusDetailView.as_view(), name='status_detail'),
+    path('detail/<int:pk>', StatusDetailView.as_view(), name='status_detail')
 ]

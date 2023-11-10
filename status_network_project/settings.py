8e468549605686496e5164d55893e74986d60536
Alex <taus.anarchy@gmail.com>
2023-11-10 17:32:50 +0200

    Change email backend to SMTP

--- status_network_project/settings.py
+++ status_network_project/settings.py
@@ -143,4 +143,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 DEFAULT_FROM_EMAIL = 'taus.anarchy@gmail.com'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = config.DEFAULT_FROM_EMAIL
+EMAIL_HOST = config.EMAIL_HOST
+EMAIL_PORT = config.EMAIL_PORT
+EMAIL_HOST_USER = config.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
+EMAIL_USE_TLS = config.EMAIL_USE_TLS
